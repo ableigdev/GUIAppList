2ce95a60ceb91af6e80dd57c607c9966e6c850d2
ableigdev <igor.yarovoy.98@ukr.net>
2018-09-24 11:41:27 +0300

    Improve adding the new student in the list

--- StudentsGUI/InputStudentInformation.cpp
+++ StudentsGUI/InputStudentInformation.cpp
@@ -26,16 +26,29 @@ void InputStudentInformation::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
 
+    CString surname = m_Student->getSurname().c_str();
+    CString name = m_Student->getName().c_str();
+    CString lastname = m_Student->getLastname().c_str();
     short int year = m_Student->getBirthYear();
     float grade = m_Student->getAverageGrade();
+
     DDX_Control(pDX, IDC_COMBO_GROUPS, m_ComboGroupList);
-    DDX_Text(pDX, IDC_STUDENT_EDIT_SURNAME, (CString)m_Student->getSurname().c_str());
-    DDX_Text(pDX, IDC_STUDENT_EDIT_NAME, (CString)m_Student->getName().c_str());
-    DDX_Text(pDX, IDC_STUDENT_EDIT_LASTNAME, (CString)m_Student->getLastname().c_str());
+    DDX_Text(pDX, IDC_STUDENT_EDIT_SURNAME, surname);
+    m_Student->setSurname(surname.GetString());
+
+    DDX_Text(pDX, IDC_STUDENT_EDIT_NAME, name);
+    m_Student->setName(name.GetString());
+
+    DDX_Text(pDX, IDC_STUDENT_EDIT_LASTNAME, lastname);
+    m_Student->setLastname(lastname.GetString());
+
     DDX_Text(pDX, IDC_STUDENT_EDIT_BIRTH_YEAR, year);
-    DDV_MinMaxUInt(pDX, m_Student->getBirthYear(), 1900, 2100);
+    DDV_MinMaxUInt(pDX, year, 1900, 2100);
+    m_Student->setBirthYear(year);
+
     DDX_Text(pDX, IDC_STUDENT_EDIT_AVERAGE_MARK, grade);
-    DDV_MinMaxFloat(pDX, m_Student->getAverageGrade(), 0.f, 5.f);
+    DDV_MinMaxFloat(pDX, grade, 0.f, 5.f);
+    m_Student->setAverageGrade(grade);
 }
 
 
@@ -82,7 +95,7 @@ void InputStudentInformation::setActiveSurname()
 {
     CWnd* wnd = GetDlgItem(IDC_STUDENT_EDIT_SURNAME);
     wnd->SetFocus();
-    ((CEdit*)(wnd))->SetSel(0, m_Student->getSurname().size());
+    ((CEdit*)(wnd))->SetSel(0, static_cast<int>(m_Student->getSurname().size()));
 }
 
 bool InputStudentInformation::checkConstruction()
@@ -94,16 +107,32 @@ bool InputStudentInformation::addStudent()
 {
     UpdateData(FALSE);
 
-    if (checkConstruction())
+    auto currentSelect = m_ComboGroupList.GetCurSel();
+
+    if (currentSelect != -1)
     {
-        if (m_ChangeFlag == ADD)
+        if (checkConstruction())
         {
-            m_Group->pushInSortList(*m_Student);
+            if (m_ChangeFlag == ADD)
+            {
+                //m_Faculty->pushInSortList(*m_Student);
+                m_Faculty->setCurrentNodeOnTheBegin();
+                for (size_t i = 0; i < m_Faculty->getSize(); ++i)
+                {
+                    if (i == currentSelect)
+                    {
+                        m_Faculty->getReferencesCurrentData().pushInSortList(*m_Student);
+                        break;
+                    }
+                    m_Faculty->operator++();
+                }
+            }
+            return true;
         }
-        return true;
+        MessageBox(__TEXT("Surname or name is empty"), __TEXT("Error"), MB_OK | MB_ICONSTOP);
     }
     m_IsModify = true;
-    MessageBox(__TEXT("Surname or name is empty"), __TEXT("Error"), MB_OK | MB_ICONSTOP);
+    MessageBox(__TEXT("Group was not chosen"), __TEXT("Error"), MB_OK | MB_ICONSTOP);
     return false;
 }
 
@@ -128,9 +157,15 @@ BOOL InputStudentInformation::OnInitDialog()
 
     setActiveSurname();
 
-    if (m_Faculty != NULL)
+    if (m_Faculty)
     {
-        m_ComboGroupList.AddString((LPCTSTR)m_Faculty->getReferencesCurrentData().getNameClassList().c_str());
+        m_Faculty->setCurrentNodeOnTheBegin();
+        for (size_t i = 0; i < m_Faculty->getSize(); ++i)
+        {
+            m_ComboGroupList.AddString((LPCTSTR)m_Faculty->getReferencesCurrentData().getNameClassList().c_str());
+            m_Faculty->operator++();
+        }
+        m_Faculty->setCurrentNodeOnTheBegin();
     }
     
     return FALSE;
@@ -168,4 +203,5 @@ void InputStudentInformation::OnBnClickedCancel()
 void InputStudentInformation::OnCbnSelchangeComboGroups()
 {
     // TODO: Add your control notification handler code here
+    auto result = m_ComboGroupList.GetCurSel();
 }
